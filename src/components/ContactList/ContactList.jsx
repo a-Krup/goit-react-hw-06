d3f419411121afbcbf0dfa4328413580c95eb17a
Andrii KRUPKO <jobsdetekt.ak@gmail.com>
2025-04-24 12:07:29 +0300

    contact list

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
 import { selectContacts } from "../../redux/contactsSlice";
+import { selectNameFilter } from "../../redux/filtersSlice";
 import styles from "./ContactList.module.css";
 
 const ContactList = () => {
   const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
     <ul className={styles.contactList}>
-      {contacts.map((contact) => (
+      {filteredContacts.map((contact) => (
         <Contact key={contact.id} contact={contact} />
       ))}
     </ul>
