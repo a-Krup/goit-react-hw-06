bdd10ee33111fd871826be10fa781245120b0100
Andrii KRUPKO <jobsdetekt.ak@gmail.com>
2025-04-24 11:17:19 +0300

    item two

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -3,25 +3,31 @@ import { nanoid } from "nanoid";
 
 const contactsSlice = createSlice({
   name: "items",
-  initialState: [
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ],
+  initialState: {
+    items: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+  },
   reducers: {
     addContact: (state, action) => {
-      state.push({
+      state.items.push({
         id: nanoid(),
         name: action.payload.name,
         number: action.payload.number,
       });
     },
-    deleteContact: (state, action) =>
-      state.filter((contact) => contact.id !== action.payload),
+    deleteContact: (state, action) => {
+      state.items = state.items.filter(
+        (contact) => contact.id !== action.payload
+      );
+    },
   },
 });
 
-export const { addContact, deleteContact } = contactsSlice.actions;
 export const selectContacts = (state) => state.items.items;
+export const { addContact, deleteContact } = contactsSlice.actions;
+
 export default contactsSlice.reducer;
